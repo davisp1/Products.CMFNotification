215cc53f53c4e8701c4d05a704fac93ced3a6b68
JC Brand <jc@opkode.com>
2012-03-29 16:40:01 +0200

    Back to development: 2.3b2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.3b1'
+version = '2.3b2dev'
 description = open('README.txt').read().strip()
 long_description = '\n\n'.join((
         open(os.path.join(
